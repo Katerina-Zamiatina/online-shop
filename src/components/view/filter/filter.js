67dc0a9df890a5a77f16b90c24f9a1a901fc7830
Katerina Zamiatina <eozamyatina@gmail.com>
2022-12-16 16:53:25 +0100

    feat: add styles to filter

--- src/components/view/filter/filter.js
+++ src/components/view/filter/filter.js
@@ -2,10 +2,9 @@ class Filter {
   draw(data) {
     const fragment = document.createDocumentFragment();
     const filterItemTemp = document.querySelector('#filter-temp');
-console.log(data)
+
     data.forEach(item => {
       const filterClone = filterItemTemp.content.cloneNode(true);
-
       filterClone.querySelector('.filter_item-name').textContent = item;
       filterClone.querySelector('.filter_item').name = item;
 
