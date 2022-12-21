a4b6d92363a2c34a1e5cf6b9472673f8a27bd5f7
Sergii Polushkin <seregapola@gmail.com>
2022-12-21 12:20:06 +0100

    feat: add search product

---
+++ src/components/pages/mainPage/products/searchProduct.js
@@ -0,0 +1,86 @@
+const productCard = document.getElementById('productCard');
+const searchBar = document.getElementById('searchBar');
+
+
+
+searchBar.addEventListener('keyup', (e) => {
+  
+    const searchString = e.target.value.toUpperCase();
+    const baseUrl = 'https://dummyjson.com/';
+    const prod = baseUrl + 'products';
+    
+    async function searchProduct(query) {
+      try {
+        const res = await fetch(`${prod}/search?q=${query}`);
+        let d = await res.json();
+         sd = await d.products;
+
+        const filteredCards = sd.filter((card) => {
+          return (
+              card.title.toUpperCase().includes(searchString) ||
+              card.brand.toUpperCase().includes(searchString) ||
+              card.price.toString().includes(searchString) ||
+              card.discountPercentage.toString().includes(searchString) ||
+              card.stock.toString().includes(searchString)
+          );
+      });
+       
+
+      function countSerch(){
+        let countNum = filteredCards.length
+        const numerSerch = `
+        <div class="foundNum" id="foundNum"> ${countNum}</div>
+        `;
+        const counter = document.getElementById("foundNum");
+              counter.innerHTML = numerSerch;
+      }
+      countSerch();
+
+
+     function renderSerchedProducts() {
+        let catalog = '';
+
+        filteredCards.forEach(({title, brand, price, discountPercentage, images, stock}) =>{
+          let nameUpperCase = title.toUpperCase();
+          let brandUpperCase = brand.toUpperCase();
+        catalog += `
+        <li class="card">
+            <div class="wrapperImg">
+              <img class="imagesProduct" src=${images[0]} alt="foto_product">
+            </div>
+            <h3 class="nameProduct">${nameUpperCase}</h3>
+            <span class="nameBrand">brand: ${brandUpperCase}</span>
+            <span class="price">price: ${price}</span>
+            <span class="discount">discount: ${discountPercentage}</span>
+            <span class="stock">stock: ${stock}</span>
+            <div class="buttonsCard">
+              <button class="addProduct">add to basket</button>
+              <button class="description">description</button>
+            </div>
+        </li>
+        `
+        });
+        const card = `
+        <ul class="wrapperProductCards" id="wrapperProductCards">
+             ${catalog}
+        </ul>
+    `;
+
+    const ROOT_PRODUCT_CARD = document.getElementById("productCard");
+          ROOT_PRODUCT_CARD.innerHTML = card;
+    };  
+    renderSerchedProducts();
+ 
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+  
+   searchProduct(searchString);
+
+  });
+
+
+
+
+
